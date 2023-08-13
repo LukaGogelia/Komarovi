d0f086e35eca3007cffedab403812b84c6d9e8f7
Nika Donadze <nik7nika7@gmail.com>
2023-08-13 21:53:16 +0400

    dashboard sidebar fixed

--- components/dashboard/Sidebar.jsx
+++ components/dashboard/Sidebar.jsx
@@ -4,27 +4,37 @@ import { sidebarItems } from "@/data/dashBoardSidebar";
 import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { ChakraProvider } from "@chakra-ui/react";
+
 export default function Sidebar() {
   const pathname = usePathname();
+
   return (
-    <div className="sidebar -dashboard">
-      {sidebarItems.map((elm, i) => (
-        <div
-          key={i}
-          className={`sidebar__item   ${
-            pathname == elm.href ? "-is-active" : ""
-          } `}
-        >
-          <Link
+    <ChakraProvider>
+      <div className="sidebar -dashboard">
+        {sidebarItems.map((elm, i) => (
+          <div
             key={i}
-            href={elm.href}
-            className="d-flex items-center text-17 lh-1 fw-500 "
+            className={`sidebar__item ${
+              pathname == elm.href ? "-is-active " : ""
+            }`}
+            style={{
+              background: pathname === elm.href ? "#2B1C55" : "inherit",
+            }}
           >
-            <i className={`${elm.iconClass} mr-15`}></i>
-            {elm.text}
-          </Link>
-        </div>
-      ))}
-    </div>
+            <Link
+              key={i}
+              href={elm.href}
+              className={`d-flex items-center text-17 lh-1 fw-500 ${
+                pathname == elm.href ? "is-active " : ""
+              }`}
+            >
+              <i className={`${elm.iconClass} mr-15`}></i>
+              {elm.text}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </ChakraProvider>
   );
 }
