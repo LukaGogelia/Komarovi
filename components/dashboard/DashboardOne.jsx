532023479def37a1ea142d5bdd60267eea12d3dd
Nika Donadze <nik7nika7@gmail.com>
2023-08-17 03:40:51 +0400

    fixed the bug

--- components/dashboard/DashboardOne.jsx
+++ components/dashboard/DashboardOne.jsx
@@ -12,7 +12,6 @@ import QuizPerformance from "./QuizPerformance";
 import { Quiz } from "@/data/mongoDb/models";
 import { QuizEntry } from "@/data/mongoDb/models";
 import mongoose from "mongoose";
-import Cards from "./Cards";
 
 export async function useFetchQuizData() {
   await mongoose.connect("mongodb://127.0.0.1:27017/komarovi", {
@@ -127,7 +126,35 @@ export default async function DashboardOne() {
             flexWrap: "wrap",
           }}
         >
-          <Cards />
+          <div
+            className=" y-gap-30 col-lg-8 col-md-12 col-sm-12 x-gap-30"
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              flexWrap: "wrap",
+            }}
+          >
+            {states.map((elm, i) => (
+              <div
+                key={i}
+                className="responsive-card col-lg-6 col-md-8 col-sm-12 "
+              >
+                <div className="d-flex justify-between items-center py-35 px-30 rounded-16 bg-white -dark-bg-dark-1 shadow-4">
+                  <div>
+                    <div className="lh-1 fw-500">{elm.title}</div>
+                    <div className="text-24 lh-1 fw-700 text-dark-1 mt-20">
+                      ${elm.value}
+                    </div>
+                    <div className="lh-1 mt-25">
+                      <span className="text-purple-1">${elm.new}</span> New
+                      Sales
+                    </div>
+                  </div>
+                  <i className={`text-40 ${elm.iconClass} text-purple-1`}></i>
+                </div>
+              </div>
+            ))}
+          </div>
 
           <ApplyGauge />
         </div>
