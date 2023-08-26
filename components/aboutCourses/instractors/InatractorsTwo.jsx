3297c4b36e71f7d6b771696573e0990ee2ca0f92
Nika Donadze <nik7nika7@gmail.com>
2023-08-26 04:16:52 +0400

    refactored models and then fixing teachers page

--- components/aboutCourses/instractors/InatractorsTwo.jsx
+++ components/aboutCourses/instractors/InatractorsTwo.jsx
@@ -5,73 +5,66 @@ import React, { useState, useEffect } from "react";
 import { categories, sortingOptions } from "@/data/instractors";
 import PaginationTwo from "@/components/common/PaginationTwo";
 import Link from "next/link";
-export default function InatractorsTwo({ teamMembers }) {
+
+export default function InatractorsTwo({ teamMembers, subjects }) {
   const [categoryOpen, setCategoryOpen] = useState(true);
+  const validTeamMembers = Array.isArray(teamMembers) ? teamMembers : [];
+  const allSubjectNames = subjects.map((subj) => subj.subject);
 
   const [filterCategories, setFilterCategories] = useState([]);
-  const [filterRatingRange, setFilterRatingRange] = useState([]);
-
+  const [filteredData, setFilteredData] = useState(validTeamMembers);
   const [currentSortingOption, setCurrentSortingOption] = useState("Default");
+  const [sortedFilteredData, setSortedFilteredData] =
+    useState(validTeamMembers);
+  useEffect(() => {
+    // Helper function to check inclusion
+    const shouldInclude = (elm) => {
+      // If there's no subjectId in the elm, exclude it
+      if (!elm.subjectId || !Array.isArray(elm.subjectId)) return false;
 
-  const [filteredData, setFilteredData] = useState([]);
+      // Check if any of the subject IDs in elm.subjectId matches an ID in subjects
+      const matches = elm.subjectId.some((subjectId) =>
+        subjects.some((subject) => subject.id === subjectId)
+      );
 
-  const [sortedFilteredData, setSortedFilteredData] = useState([]);
+      return matches;
+    };
 
-  const [pageNumber, setPageNumber] = useState(1);
+    // If filterCategories is empty, simply use validTeamMembers
+    const refItems =
+      filterCategories.length > 0
+        ? validTeamMembers.filter(shouldInclude)
+        : validTeamMembers;
+
+    setFilteredData(refItems);
+  }, [filterCategories, validTeamMembers, subjects]);
 
   useEffect(() => {
-    let refItems = [];
-    if (filterCategories.length > 0) {
-      const filtered = teamMembers.filter((elm) =>
-        filterCategories.includes(elm.category)
-      );
-      refItems = filtered;
-    } else {
-      refItems = teamMembers;
-    }
-
-    let filteredArrays = [];
-
-    if (filterRatingRange.length > 0) {
-      const filtered = refItems.filter(
-        (elm) =>
-          elm.rating >= filterRatingRange[0] &&
-          elm.rating <= filterRatingRange[1]
-      );
-      filteredArrays = [...filteredArrays, filtered];
-    }
-
-    const commonItems = refItems.filter((item) =>
-      filteredArrays.every((array) => array.includes(item))
-    );
-    setFilteredData(commonItems);
-    setPageNumber(1);
-  }, [filterCategories, filterRatingRange]);
-
-  useEffect(() => {
-    if (currentSortingOption == "Default") {
-      setSortedFilteredData(filteredData);
-    } else if (currentSortingOption == "Rating (asc)") {
-      setSortedFilteredData(
-        [...filteredData].sort((a, b) => a.rating - b.rating)
-      );
-    } else if (currentSortingOption == "Rating (dsc)") {
-      setSortedFilteredData(
-        [...filteredData].sort((a, b) => b.rating - a.rating)
-      );
+    switch (currentSortingOption) {
+      case "Rating (asc)":
+        setSortedFilteredData(
+          [...filteredData].sort((a, b) => a.rating - b.rating)
+        );
+        break;
+      case "Rating (dsc)":
+        setSortedFilteredData(
+          [...filteredData].sort((a, b) => b.rating - a.rating)
+        );
+        break;
+      default:
+        setSortedFilteredData(filteredData);
+        break;
     }
   }, [currentSortingOption, filteredData]);
 
   const handleFilterCategories = (item) => {
     if (filterCategories.includes(item)) {
-      const filtered = filterCategories.filter((elm) => elm != item);
-      setFilterCategories([...filtered]);
+      setFilterCategories((prev) =>
+        prev.filter((category) => category !== item)
+      );
     } else {
-      setFilterCategories((pre) => [...pre, item]);
+      setFilterCategories((prev) => [...prev, item]);
     }
-  };
-  const handleFilterRatingRange = (item) => {
-    setFilterRatingRange(item);
   };
 
   return (
@@ -113,7 +106,7 @@ export default function InatractorsTwo({ teamMembers }) {
                         className="accordion__button items-center"
                         onClick={() => setCategoryOpen((pre) => !pre)}
                       >
-                        <h5 className="sidebar__title">Category</h5>
+                        <h5 className="sidebar__title">Subjects</h5>
 
                         <div className="accordion__icon">
                           <div className="icon icon-chevron-down"></div>
@@ -146,51 +139,41 @@ export default function InatractorsTwo({ teamMembers }) {
                               <div className="sidebar-checkbox__title">All</div>
                               <div className="sidebar-checkbox__count"></div>
                             </div>
-                            {categories.map((elm, i) => (
+                            {allSubjectNames.map((subjectName, i) => (
                               <div
                                 key={i}
                                 onClick={() =>
-                                  handleFilterCategories(elm.title)
+                                  handleFilterCategories(subjectName)
                                 }
                                 className="sidebar-checkbox__item cursor"
                               >
                                 <div className="form-checkbox">
                                   <input
                                     type="checkbox"
-                                    checked={
-                                      filterCategories.includes(elm.title)
-                                        ? true
-                                        : false
-                                    }
+                                    checked={filterCategories.includes(
+                                      subjectName
+                                    )}
                                   />
                                   <div className="form-checkbox__mark">
                                     <div className="form-checkbox__icon icon-check"></div>
                                   </div>
                                 </div>
-
                                 <div className="sidebar-checkbox__title">
-                                  {elm.title}
+                                  {subjectName}
                                 </div>
                                 <div className="sidebar-checkbox__count">
                                   (
                                   {
                                     teamMembers.filter(
-                                      (itm) => itm.category == elm.title
+                                      (itm) =>
+                                        itm.subject &&
+                                        itm.subject.subject === subjectName // Updated itm.subject.name to itm.subject.subject
                                     ).length
                                   }
                                   )
                                 </div>
                               </div>
                             ))}
-                          </div>
-
-                          <div className="sidebar__more mt-15">
-                            <a
-                              href="#"
-                              className="text-14 fw-500 underline text-purple-1"
-                            >
-                              Show more
-                            </a>
                           </div>
                         </div>
                       </div>
@@ -280,58 +263,55 @@ export default function InatractorsTwo({ teamMembers }) {
               </div>
 
               <div className="row y-gap-30">
-                {sortedFilteredData
-                  .slice((pageNumber - 1) * 6, pageNumber * 6)
-                  .map((elm, i) => (
-                    <div key={i} className="col-lg-4 col-md-6">
-                      <div className="teamCard -type-1 px-10 py-10 rounded-8 border-light">
-                        <div className="teamCard__image">
-                          <Image
-                            width={488}
-                            height={537}
-                            src={elm.image}
-                            alt="image"
-                          />
-                        </div>
-                        <div className="teamCard__content mt-10 px-10 pb-5">
-                          <h4 className="teamCard__title">
-                            <Link
-                              className="linkCustom"
-                              href={`/instructors/${elm.id}`}
-                            >
-                              {elm.name}
-                            </Link>
-                          </h4>
-                          <p className="teamCard__text">{elm.role}</p>
-                          <div className="d-flex x-gap-10 pt-10">
-                            <div className="d-flex items-center">
-                              <div className="icon-star text-yellow-1 text-14"></div>
-                              <div className="text-13 lh-1 ml-8">
-                                {elm.rating}
-                              </div>
+                {sortedFilteredData.map((elm, i) => (
+                  <div key={i} className="col-lg-4 col-md-6">
+                    <div className="teamCard -type-1 px-10 py-10 rounded-8 border-light">
+                      <div className="teamCard__image">
+                        <Image
+                          width={488}
+                          height={537}
+                          src={elm.image}
+                          alt="image"
+                        />
+                      </div>
+                      <div className="teamCard__content mt-10 px-10 pb-5">
+                        <h4 className="teamCard__title">
+                          <Link
+                            className="linkCustom"
+                            href={`/instructors/${elm.id}`}
+                          >
+                            {elm.name}
+                          </Link>
+                        </h4>
+                        <p className="teamCard__text">{elm.role}</p>
+                        <div className="d-flex x-gap-10 pt-10">
+                          <div className="d-flex items-center">
+                            <div className="text-13 lh-1 ml-8">
+                              {elm.rating}
                             </div>
+                          </div>
 
-                            <div className="d-flex items-center">
-                              <div className="icon-person-3 text-14"></div>
-                              <div className="text-13 lh-1 ml-8">
-                                {elm.students} Students
-                              </div>
+                          <div className="d-flex items-center">
+                            <div className="icon-person-3 text-14"></div>
+                            <div className="text-13 lh-1 ml-8">
+                              {elm.students} Classes
                             </div>
+                          </div>
 
-                            <div className="d-flex items-center">
-                              <div className="icon-play text-14"></div>
-                              <div className="text-13 lh-1 ml-8">
-                                {elm.courses} Course
-                              </div>
+                          <div className="d-flex items-center">
+                            <div className="icon-play text-14"></div>
+                            <div className="text-13 lh-1 ml-8">
+                              {elm.courses} subject
                             </div>
                           </div>
                         </div>
                       </div>
                     </div>
-                  ))}
+                  </div>
+                ))}
               </div>
 
-              <div className="row justify-center pt-60 lg:pt-40">
+              {/* <div className="row justify-center pt-60 lg:pt-40">
                 <div className="col-auto">
                   <PaginationTwo
                     pageNumber={pageNumber}
@@ -340,7 +320,7 @@ export default function InatractorsTwo({ teamMembers }) {
                     pageCapacity={6}
                   />
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
